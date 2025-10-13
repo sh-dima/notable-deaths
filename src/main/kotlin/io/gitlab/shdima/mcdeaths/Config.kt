4b3615d8cddf5d8373a035063e4ecedca2e1858d
Дима Ш. <dima.o.sh@proton.me>
2025-10-13 17:24:36 +0100

    Implement basic functionality
    * Send death messages to all players on the server depending on if they are enabled in the configuration for the given entity.

--- src/main/kotlin/io/gitlab/shdima/mcdeaths/Config.kt
+++ src/main/kotlin/io/gitlab/shdima/mcdeaths/Config.kt
@@ -1,7 +1,15 @@
 package io.gitlab.shdima.mcdeaths
 
 import de.exlll.configlib.Configuration
+import org.bukkit.entity.EntityType
+
+@Configuration
+data class EntityConfiguration(var enabled: Boolean = false, var location: Boolean = false)
 
 @Configuration
 class Config {
+    var mobs: Map<EntityType, EntityConfiguration> = mapOf(
+        Pair(EntityType.VILLAGER, EntityConfiguration(enabled = true, location = false)),
+        Pair(EntityType.WANDERING_TRADER, EntityConfiguration(enabled = true, location = false)),
+    )
 }
--- src/main/kotlin/io/gitlab/shdima/mcdeaths/NotableDeaths.kt
+++ src/main/kotlin/io/gitlab/shdima/mcdeaths/NotableDeaths.kt
@@ -2,13 +2,18 @@ package io.gitlab.shdima.mcdeaths
 
 import de.exlll.configlib.NameFormatters
 import de.exlll.configlib.YamlConfigurationProperties
-import org.bstats.bukkit.Metrics
 import de.exlll.configlib.YamlConfigurations
+import org.bstats.bukkit.Metrics
+import org.bukkit.craftbukkit.entity.CraftLivingEntity
+import org.bukkit.craftbukkit.entity.CraftPlayer
+import org.bukkit.event.EventHandler
+import org.bukkit.event.Listener
+import org.bukkit.event.entity.EntityDeathEvent
 import org.bukkit.plugin.java.JavaPlugin
 import java.nio.file.Path
 
 @Suppress("unused")
-class NotableDeaths : JavaPlugin() {
+class NotableDeaths : JavaPlugin(), Listener {
 
     private lateinit var config: Config
 
@@ -24,12 +29,37 @@ class NotableDeaths : JavaPlugin() {
             Config()
         }
 
-//        YamlConfigurations.save(configFile, Config::class.java, config, configProperties)
+        YamlConfigurations.save(configFile, Config::class.java, config, configProperties)
 
         try {
             Metrics(this, 27578)
         } catch (exception: Exception) {
             exception.printStackTrace()
+        }
+
+        server.pluginManager.registerEvents(this, this)
+    }
+
+    @EventHandler
+    private fun onDeath(event: EntityDeathEvent) {
+        val entity = event.entity
+        val type = entity.type
+
+        val entityConfig = config.mobs[type] ?: return
+        val enabled = entityConfig.enabled
+        if (!enabled) return
+
+        val nmsEntity = (entity as CraftLivingEntity).handle
+
+        val deathMessage = nmsEntity.combatTracker.deathMessage.copy()
+
+        server.onlinePlayers.forEach {
+            val serverPlayer = (it as CraftPlayer).handle
+
+            serverPlayer.displayClientMessage(
+                deathMessage,
+                false, // Whether the message is an action bar message
+            )
         }
     }
 }
