c3022f1dfdd87d08586d0cda889b2167ce63b21a
Дима Ш. <dima.o.sh@proton.me>
2025-10-13 19:39:30 +0100

    Add config docs

--- src/main/kotlin/io/gitlab/shdima/mcdeaths/Config.kt
+++ src/main/kotlin/io/gitlab/shdima/mcdeaths/Config.kt
@@ -1,13 +1,24 @@
 package io.gitlab.shdima.mcdeaths
 
+import de.exlll.configlib.Comment
 import de.exlll.configlib.Configuration
 import org.bukkit.entity.EntityType
 
 @Configuration
-data class EntityConfiguration(var all: Boolean = false, var named: Boolean = false, var tamed: Boolean = false, var location: Boolean = false)
+data class EntityConfiguration(
+    @Comment("Whether death messages should be announced for all entities of this type")
+    var all: Boolean = false,
+    @Comment("Whether death messages should be announced for named entities of this type")
+    var named: Boolean = false,
+    @Comment("Whether death messages should be announced for tamed entities of this type")
+    var tamed: Boolean = false,
+    @Comment("Whether the location should be broadcast along with the death message for entities of this type")
+    var location: Boolean = false,
+)
 
 @Configuration
 class Config {
+    @Comment("The default configuration for mobs\nConfiguration under 'mobs' overrides this")
     var default = EntityConfiguration(
         all = false,
         named = true,
@@ -15,6 +26,7 @@ class Config {
         location = false,
     )
 
+    @Comment("Overrides for specific entities")
     var mobs: Map<EntityType, EntityConfiguration> = mapOf(
         Pair(EntityType.VILLAGER, EntityConfiguration(all = true, location = false)),
         Pair(EntityType.WANDERING_TRADER, EntityConfiguration(all = true, location = false)),
